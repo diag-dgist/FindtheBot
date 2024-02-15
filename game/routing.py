6ddfba9f076dbc4105c138bd2acae1d7684dbebd
Ahyeon <ahyeonshin79@gmail.com>
2024-02-15 16:36:42 +0900

    Tutorial K->E(done)

--- game/routing.py
+++ game/routing.py
@@ -1,12 +1,12 @@
 """
 routing for websocket
 
-game/routing.py에서 사용한 ws_urlpatterns를 정의.
+[ny]game/routing.py에서 사용한 ws_urlpatterns를 정의.
 
 """
 from django.urls import path
 from . import consumers
 
-ws_urlpatterns = [ #ws의 url pattern에 따라 어떤 consumer로 routing 할지 path 지정
+ws_urlpatterns = [      #[ny]ws의 url pattern에 따라 어떤 consumer로 routing 할지 path 지정
     path('ws/game/<room_num>/<username>/<team_id>', consumers.RoomConsumer.as_asgi()), 
 ]
